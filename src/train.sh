ab6686dbcad4e97ab36379df1013434f0c7c468c
Cody <codydong@sas.upenn.edu>
2023-01-22 23:05:12 -0500

    amend bash file

--- src/train.sh
+++ src/train.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-#SBATCH --time=19:55:00
-#SBATCH --constraint=1
-#SBATCH --mem-per-cpu=12G
-#SBATCH --job-name=when
-#SBATCH --mail-type=begin
-#SBATCH --mail-type=end
-#SBATCH --mail-type=FAIL
-#SBATCH --mail-user=codydong@princeton.edu
-#SBATCH --output=slurm_log/when-%j.log
+#SBATCH -t 11:55:00
+#SBATCH -c 1
+#SBATCH --mem-per-cpu 12G
+
+#SBATCH --job-name=cody-exp-2
+#SBATCH --output slurm_log/%j.log
 
 module purge
 module load anaconda3/2021.11
